6a8d301d690ee790bb2caaaf13c18e131ab3a3a2
Michael Chong <michael.yc.chong@gmail.com>
2021-11-25 16:15:05 -0500

    fixes/cleaning discussed in meeting

--- scripts/data_merge.R
+++ scripts/data_merge.R
@@ -65,6 +65,7 @@ sc_decisions <- sc_decisions_raw %>%
     term, 
     naturalCourt, 
     chief, 
+    jurisdiction,
     dateArgument,
     dateRearg,
     petitioner,
@@ -120,15 +121,52 @@ sc_decisions$adminAction <- with(party, PartyName[match(sc_decisions$adminAction
 sc_decisions$caseOrigin <- with(caseOrigin, CaseOriginName[match(sc_decisions$caseOrigin, CaseOrigin)])
 sc_decisions$caseSource <- with(caseOrigin, CaseOriginName[match(sc_decisions$caseSource, CaseOrigin)])
 
+# If a case has no origin/source, then it was in the original jurisdiction
+sc_decisions <- sc_decisions %>%
+  mutate(
+    caseOrigin = ifelse(is.na(caseOrigin), "No other court involved", caseOrigin),
+    caseSource = ifelse(is.na(caseSource), "No other court involved", caseSource)) 
+
+# Combine caseOrigin/caseOriginState, caseSource/caseSourceState
+sc_decisions <- sc_decisions %>%
+  mutate(caseOrigin = ifelse(
+    !is.na(caseOriginState),
+    str_c(caseOrigin, ", ", caseOriginState),
+    caseOrigin
+  )) %>%
+  mutate(caseSource = ifelse(
+    !is.na(caseSourceState),
+    str_c(caseSource, ", ", caseSourceState),
+    caseSource
+  )) %>%
+  select(-caseOriginState, -caseSourceState)
+
 # Mapping lower court disposition column
 sc_decisions$lcDisposition <- with(lcDisposition, lcDispositionName[match(sc_decisions$lcDisposition, lcDisposition)])
 
+# if lcDisposition is NA, then check jurisdiction
+# If jurisdiction = 9, then it is in Supreme Court's original jurisdiction
+# If other, then it came from a trial court
+sc_decisions <- sc_decisions %>%
+  mutate(lcDisposition = ifelse(
+    !is.na(lcDisposition),
+    lcDisposition,
+    ifelse(jurisdiction == 0, "NA: SC original jurisdiction", "NA: trial court")
+  ))
+
 # Mapping cert reason column
+# If certReason is NA, assume it is "other reason" (13)
+sc_decisions <- sc_decisions %>%
+  mutate(certReason = ifelse(is.na(certReason), 13, certReason))
+
 sc_decisions$certReason <- with(certReason, CertReasonName[match(sc_decisions$certReason, CertReason)])
 
 # Mapping lower court disposition direction
 sc_decisions$lcDispositionDirection <- ifelse(sc_decisions$lcDispositionDirection == 1, "Conservative", 
-                                              ifelse(sc_decisions$lcDispositionDirection == 2, "Liberal", "Unspecified"))
+                                              ifelse(sc_decisions$lcDispositionDirection == 2, "Liberal", "Not applicable"))
+
+sc_decisions <- sc_decisions %>%
+  mutate(lcDispositionDirection = ifelse(is.na(lcDispositionDirection), "Not applicable", lcDispositionDirection))
 
 # Calculate judge's age at decision year
 sc_decisions$justiceAge <- as.numeric(sc_decisions$yearDecision) - as.numeric(sc_decisions$justiceBirthYear)
@@ -162,5 +200,40 @@ sc_decisions <- sc_decisions %>%
   left_join(presidents, by = c("presidentDate" = "startDate")) 
 
 sc_decisions <- sc_decisions %>%
-  select(-contains("start"), -contains("end"))
+  select(-contains("start"), -contains("end"), -congress)
+
+# Other variable alterations
+sc_decisions <- sc_decisions %>%
+  # If threeJudgeFdc is missing, assume it was not heard at a 3 judge federal court
+  mutate(threeJudgeFdc = ifelse(is.na(threeJudgeFdc), 0, threeJudgeFdc)) %>%
+  # Turn adminAction into boolean 
+  mutate(adminActionBool = !is.na(adminAction)) %>%
+  # Were there oral arguments?
+  mutate(oralArgBool = !is.na(dateArgument)) %>%
+  # Was there re-argument?
+  mutate(reargBool = !is.na(dateRearg)) %>%
+  # Calculate time to decision 
+  mutate(
+    dateArgument = as.Date(dateArgument, format = "%m/%d/%Y"),
+    decisionTime = ifelse(
+      oralArgBool,
+     dateDecision - dateArgument, 
+      0)) %>%
+  # Check whether case came from Justice's former circuit
+  mutate(
+    formerCourt = str_detect(caseSource, justiceLowerCourt),
+    formerCourt = ifelse(is.na(formerCourt), FALSE, TRUE)
+  ) %>%
+  # remove columns not to be used in model
+  select(-jurisdiction, 
+         -adminAction, 
+         -adminActionState, 
+         -justiceLowerCourt, 
+         -dateArgument, 
+         -dateRearg,
+         -petitionerState,
+         -respondentState)
+  
+
+
 
